649c3cea6b3742dfdb9906ad8746206f6f480cba
mohamed ameer <mohamedameer577@gmail.com>
2022-06-23 20:26:12 +0200

    first

---
+++ app_users/forms.py
@@ -0,0 +1,82 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from app_users.models import Profile
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta():
+        model = User
+        fields = ('username','first_name','last_name', 'email', 'password1', 'password2')
+
+        # widgets = {
+        # "password":"forms.PasswordInput()",
+        # }
+
+        labels = {
+        'password1':'Password',
+        'password2':'Confirm Password'
+        }
+
+class UserProfileInfoForm(forms.ModelForm):
+    # teacher = 'teacher'
+    # student = 'student'
+    # user_types = [
+    #     (student, 'student'),
+    #     (teacher, 'teacher'),
+    # ]
+    # user_type = forms.ChoiceField(required=True, choices=user_types)
+    # # user_type = forms.CheckboxInput(required=True, choices=user_types)
+
+    class Meta():
+        model = Profile
+        fields = ('user_type',)
+
+##############################
+
+class ChangePasswordForm(forms.ModelForm):
+    id = forms.CharField(widget=forms.HiddenInput())
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input is-medium'}), label="Old password", required=True)
+    new_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input is-medium'}), label="New password", required=True)
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input is-medium'}), label="Confirm new password", required=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'old_password', 'new_password', 'confirm_password')
+
+    def clean(self):
+        super(ChangePasswordForm, self).clean()
+        id = self.cleaned_data.get('id')
+        old_password = self.cleaned_data.get('old_password')
+        new_password = self.cleaned_data.get('new_password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        user = User.objects.get(pk=id)
+        if not user.check_password(old_password):
+            self._errors['old_password'] =self.error_class(['Old password do not match.'])
+        if new_password != confirm_password:
+            self._errors['new_password'] =self.error_class(['Passwords do not match.'])
+        return self.cleaned_data
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields= ['username','first_name','last_name','email'] 
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields =['picture','phone','college_id','facebook','twitter','instgram']
+
+
+# class EditProfileForm(forms.ModelForm):
+# 	first_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
+# 	last_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
+# 	picture = forms.ImageField(required=False)
+# 	banner = forms.ImageField(required=False)
+# 	phone = forms.IntegerField(widget=forms.TextInput(), required=False)
+# 	college_id = forms.IntegerField(widget=forms.TextInput(), required=False)
+# 	class Meta:
+# 		model = Profile
+# 		fields = ('picture', 'banner', 'first_name', 'last_name','phone','college_id')
+# 		# fields = ('picture', 'banner', 'first_name', 'last_name', 'location', 'url', 'profile_info')
---
+++ app_users/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.contrib.auth.models import User
+import os
+from django.db.models.signals import post_save
+from PIL import Image
+from django.conf import settings
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+User._meta.get_field('email')._unique = True
+User._meta.get_field('email').blank = False
+User._meta.get_field('email').null = False
+
+def user_directory_path_profile(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    profile_pic_name = 'user_{0}/profile.jpg'.format(instance.user.id)
+    full_path = os.path.join(settings.MEDIA_ROOT, profile_pic_name)
+
+    if os.path.exists(full_path):
+        os.remove(full_path)
+        
+    return profile_pic_name
+
+def user_directory_path_banner(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    banner_pic_name = 'user_{0}/banner.jpg'.format(instance.user.id)
+    full_path = os.path.join(settings.MEDIA_ROOT, banner_pic_name)
+
+    if os.path.exists(full_path):
+        os.remove(full_path)
+
+    return banner_pic_name
+user_types = (
+    ('student', 'student'),
+    ('teacher', 'teacher'),
+)
+class Profile(models.Model):
+    user = models.OneToOneField(User,primary_key=True, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to=user_directory_path_profile, blank=True, null=True, verbose_name='Picture')
+    phone = models.CharField(max_length=15)
+    college_id = models.CharField(max_length=20)
+    facebook =models.URLField(max_length=200, blank=True)
+    twitter =models.URLField(max_length=200, blank=True)
+    instgram =models.URLField(max_length=200, blank=True)
+    # teacher = 'teacher'
+    # student = 'student'
+    # user_types = [
+    #     (student, 'student'),
+    #     (teacher, 'teacher'),
+    # ]
+    user_type = models.CharField(max_length=10, choices=user_types, default='student')
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        SIZE = 250, 250
+
+        if self.picture:
+            pic = Image.open(self.picture.path)
+            pic.thumbnail(SIZE, Image.LANCZOS)
+            pic.save(self.picture.path)
+
+    def __str__(self):
+        return self.user.username
+
+class Contact(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    feedback = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('index')
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
