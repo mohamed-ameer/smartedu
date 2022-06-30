38b56321deead73d6032955c166050b6255c81e7
mohamed ameer <mohamedameer577@gmail.com>
2022-06-30 16:08:42 +0200

    add3

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from quiz.models import Quizzes, Question, Answer
-
+from django.forms.widgets import NumberInput
 class NewQuizForm(forms.ModelForm):
 	title = forms.CharField(widget=forms.TextInput(attrs={'class': 'validate'}), required=True)
-	due = forms.DateField(widget=forms.TextInput(attrs={'class': 'datepicker'}), required=True)
+	due = forms.DateField(widget=NumberInput(attrs={'type': 'date'}), required=True)
 	time_limit_mins = forms.IntegerField(max_value=360, min_value=10)
 
 	class Meta:
