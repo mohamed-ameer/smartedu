ecb0ea39d16ca5b70f2e3c17f1b0c541d666e952
mohamed ameer <mohamedameer577@gmail.com>
2022-07-02 17:31:17 +0200

    scheduale

--- scheduale/views.py
+++ scheduale/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def Scheduale(request):
-
-    return render(request, 'classroom/Scheduale.html')
+    assignments=AssignmentScheduale.objects.all()
+    quizes=QuizScheduale.objects.all()
+    context = {
+        'assignments': assignments,'quizes': quizes
+    }    
+    return render(request, 'classroom/Scheduale.html',context)
