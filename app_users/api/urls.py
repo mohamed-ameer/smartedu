7d3fd5f07d4dff162623f2bdaec364c2ec152374
mohamed ameer <mohamedameer577@gmail.com>
2022-07-03 13:58:52 +0200

    Tokensauthdone

--- app_users/api/urls.py
+++ app_users/api/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('signup/',SignupView.as_view()),#post 
+    path('login/',CustomAuthToken.as_view(), name='auth-token'),#post
+    path('logout/', LogoutView.as_view(), name='logout-view'),#post
+
 ]
--- app_users/api/views.py
+++ app_users/api/views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, permissions, status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import BasePermission,AllowAny, SAFE_METHODS
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from .serializers import *
@@ -21,3 +21,25 @@ class SignupView(generics.GenericAPIView):
             "token":Token.objects.get(user=user).key,
             "message":"account created successfully"
         })
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer=self.serializer_class(data=request.data, context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        user=serializer.validated_data['user']
+        token, created=Token.objects.get_or_create(user=user)
+        profile, created=Profile.objects.get_or_create(user=user)
+        return Response({
+            'token':token.key,
+            'user_id':user.pk,
+            'profile':profile.user_type
+        })
+
+
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        request.auth.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
